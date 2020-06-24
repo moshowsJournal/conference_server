bc5e1716bd448ca4df662c16cb1a01ab7a7b0561
Moshow <withyouamalive@gmail.com>
2020-06-24 22:45:51 +0100

    added cors

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,6 +10,10 @@ class PagesController extends Controller
 {
     //
 
+    public function __construct(){
+        $this->middleware('cors');
+    }
+
     public function add_talk(Request $request){
         $is_valid = Talk::is_valid();
         $response = array(
